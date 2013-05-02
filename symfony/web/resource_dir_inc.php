25987db29f6e9d77c565586df34fc88780b1a8a7
Sunil Enadle <sunil@synerzip.com>
2013-05-02 17:11:44 +0530

    Adding ornageHRM-3.0.1 source code

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_513fd0981da216.38969927/css');
+sfConfig::set('sf_web_js_dir_name','webres_513fd0981da216.38969927/js');
+sfConfig::set('sf_web_images_dir_name','webres_513fd0981da216.38969927/images');
+sfConfig::set('ohrm_resource_dir', 'webres_513fd0981da216.38969927');
